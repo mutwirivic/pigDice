b0a945c7134bbb957b0f27dc18a5446e666e3b31
mutwirivic <mutwirivic1@gmail.com>
2018-04-11 21:30:40 +0300

    Roll button working for both players

--- js/scripts.js
+++ js/scripts.js
@@ -1,16 +1,43 @@
-
-
+function resetFields(){
+ $("#playerOneName").val('');
+ $("#playerTwoName").val('');
+}
+var rollDice = function () {
+  return Math.floor(6*Math.random()+1);
+}
 //FRONT-END
 $(document).ready(function(){
 
-$("#nameForm").submit(function(event){
-  event.preventDefault();
-  var player1=$("#playerOneName").val();
-  console.log(player1);
-  var player2=$("#playerTwoName").val();
-  console.log(player2)
-})
+  $("#nameForm").submit(function(event){
+    event.preventDefault();
+    var player1=$("#playerOneName").val();
+    var player2=$("#playerTwoName").val();
+    resetFields();
+
+    $("#nameForm").hide();
+    $("#player-section").show();
+    $("#playerOneShow").text(player1);
+    $("#playerTwoShow").text(player2);
+
+  })
 
 
+  $("#new-game").click(function(){
+    $("#nameForm").show();
+    $("#player-section").hide();
+  });
 
+
+  //roll dice 1
+  $("#playerOneRollBtn").click(function(){
+    var player1roll=rollDice();
+    //console.log(player1roll);
+    $("#playerOneRoll").text(player1roll);
+  })
+
+  //roll dice two
+  $("#playerTwoRollBtn").click(function(){
+    var player2roll=rollDice();
+    $("#playerTwoRoll").text(player2roll);
+  })
 });
